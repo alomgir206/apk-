1bf57a8eaea9d9cc93c182c1a3a90a4de1633dfa
alomgir206 <113690408+alomgir206@users.noreply.github.com>
2022-09-16 17:47:24 +0600

    Add files via upload

---
+++ kalishell.py
@@ -0,0 +1,15 @@
+l =  '\033[91m'
+s = '\33[94m'+'\033[94m'+'\033[91m'+'\33[97m'+'\33[93m'+'\033[1;32m'+'\033[0m'
+
+i =s+str("┌")
+ii =l+str("kali")
+iii =s+str("💀")
+iv =l+str("localhost")
+v =s+str("-[")
+vi =l+str("~")
+vii =s+str("]")
+viii =s+str("└")
+ix =s+str("#")
+
+print(i+ii+iii+iv+v+vi+vii)
+print(viii+ix)
